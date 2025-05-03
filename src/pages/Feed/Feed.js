2b83eaa50a91b479e38f5030f14409ba0ebd4a8b
Guy Arbilly <Guy.Arbilly@gmail.com>
2025-05-03 13:42:59 +0300

    hapter 27 - section 416 - add support for update post based on webs socjet updates

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -43,6 +43,8 @@ class Feed extends Component {
     socket.on('posts', data => {
       if (data.action === 'create') {
         this.addPost(data.post);
+      } else if (data.action === 'update') {
+        this.updatePost(data.post);
       }
     })
   }
@@ -59,6 +61,19 @@ class Feed extends Component {
       return {
         posts: updatedPosts,
         totalPosts: prevState.totalPosts + 1
+      };
+    });
+  }
+
+  updatePost = updatedPost => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      const postIndex = updatedPosts.findIndex(p => p._id === updatedPost._id);
+      if (postIndex > -1) {
+        updatedPosts[postIndex] = updatedPost;
+      }
+      return {
+        posts: updatedPosts
       };
     });
   }
@@ -188,17 +203,17 @@ class Feed extends Component {
           createdAt: resData.post.createdAt
         };
         this.setState(prevState => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              p => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          // } else if (prevState.posts.length < 2) {
-          //   updatedPosts = prevState.posts.concat(post);
-          }
+          // let updatedPosts = [...prevState.posts];
+          // if (prevState.editPost) {
+          //   const postIndex = prevState.posts.findIndex(
+          //     p => p._id === prevState.editPost._id
+          //   );
+          //   updatedPosts[postIndex] = post;
+          // // } else if (prevState.posts.length < 2) {
+          // //   updatedPosts = prevState.posts.concat(post);
+          // }
           return {
-            posts: updatedPosts,
+            //posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
