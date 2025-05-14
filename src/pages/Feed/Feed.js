38e3c5f8bb1633d13a7e87120d1dbb310012a951
guy arbily <guy.arbilly@gmail.com>
2025-05-14 21:18:09 +0300

    Chapter 28 - section 424 - part 1 - remove socket support and remove  addPost and updatePost called from socket.io

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import openSocket from 'socket.io-client';
+
 import Post from '../../components/Feed/Post/Post';
 import Button from '../../components/Button/Button';
 import FeedEdit from '../../components/Feed/FeedEdit/FeedEdit';
@@ -39,45 +39,6 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    const socket = openSocket('http://localhost:8080');
-    socket.on('posts', data => {
-      if (data.action === 'create') {
-        this.addPost(data.post);
-      } else if (data.action === 'update') {
-        this.updatePost(data.post);
-      } else if (data.action === 'delete') {
-        this.loadPosts();
-      }
-    })
-  }
-
-  addPost = post =>{
-    this.setState(prevState => {
-      const updatedPosts = [...prevState.posts];
-      if (prevState.postPage === 1) {
-        if (prevState.posts.length >= 2) {
-          updatedPosts.pop();
-        }
-        updatedPosts.unshift(post);
-      }
-      return {
-        posts: updatedPosts,
-        totalPosts: prevState.totalPosts + 1
-      };
-    });
-  }
-
-  updatePost = updatedPost => {
-    this.setState(prevState => {
-      const updatedPosts = [...prevState.posts];
-      const postIndex = updatedPosts.findIndex(p => p._id === updatedPost._id);
-      if (postIndex > -1) {
-        updatedPosts[postIndex] = updatedPost;
-      }
-      return {
-        posts: updatedPosts
-      };
-    });
   }
 
   loadPosts = direction => {
